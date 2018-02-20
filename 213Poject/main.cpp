4e5b78a5bd3a80e75c8284e95ba6db463eeed29a
TommyYangHui <yanghui@sugrsugr.com>
2018-02-20 22:09:13 +0800

    face match sus

--- 213Poject/main.cpp
+++ 213Poject/main.cpp
@@ -40,45 +40,124 @@ int main()
 	
 	
 	//-------------------------------------
+	// 带参数调用人脸检测
+	{
 	Json::Value result;
 
 	std::string image;
-	aip::get_file_content("photo/hansum.JPG", &image);
+	aip::get_file_content("photo/WZT_227552.JPG", &image);
 	// 如果有可选参数
 	std::map<std::string, std::string> options;
 	options["max_face_num"] = "2";
 	options["face_fields"] = "age,beauty,expression,faceshape,gender,glasses,race,qualities";
 
-	// 带参数调用人脸检测
+	
 	result = client.detect(image, options);
-	
-/* 	std::cout <<"error_msg = " <<result["error_msg"].asString()<< endl;  
-	std::cout <<"error_code = "<< result["error_code"].asInt()<< endl;
-	std::cout <<"result_num = "<< result["result_num"].asInt()<< endl; */
-	
-	
-	Json::Reader reader;
-	Json::FastWriter fwriter;
 	Json::StyledWriter swriter;
-//need add a condition :promise the result isn't null
-/*	std::string _str = fwriter(result);
-	std::ofstream ofs("example_fast_writer.json");
-	ofs << _str;
-	ofs.close();
-
-	_str = swriter(result);
-	ofs.open("example_styled_writer.json");
-	ofs << _str;
-	ofs.close();
-	
-*/
+	//need add a condition :promise the result isn't null
 	std::cout << swriter.write(result) << std::endl;	
+	}
+	//-------end------------------------------
+	
+	//-------------------------------------
+	//face user add:
+/* 	{
+	Json::Value result_user_add;
+
+	std::string uid = "gakki";
+
+	std::string user_info = "100,200";
+
+	std::string group_id = "group1,group2";
+
+	std::string image_a;
+	Json::StyledWriter swriter;
+	
+	aip::get_file_content("photo/gakki (2).jpg", &image_a);
+
+	// 如果有可选参数
+	std::map<std::string, std::string> options_a;
+	options_a["action_type"] = "append";
+
+	// 带参数调用人脸注册
+	result_user_add = client.user_add(uid, user_info, group_id, image_a, options_a);
+	std::cout << swriter.write(result_user_add) << std::endl;	
+	} */
+	//-------end---------------------------
+	
+	//--------------------------------------
+	/* // 调用用户信息查询
+	{
+	Json::Value result_b;
+	Json::StyledWriter swriter;
+	result_b = client.user_get("YangHui", aip::null);
+	std::cout << swriter.write(result_b) << std::endl;	
+	
+	// 调用用户信息查询
+	Json::Value result_c;
+	result_c = client.user_get("gakki", aip::null);
+	std::cout << swriter.write(result_c) << std::endl;	
+	} */
+	//-------end---------------------------
+	
+	//--------------------------------------
+	// face identify
+	{
+	Json::Value result;
+
+	std::string group_id = "group1,group2";
+
+	std::string image;
+	Json::StyledWriter swriter;
+	aip::get_file_content("photo/WZT_227552.JPG", &image);
+
+	//调用人脸识别
+	//result = client.identify(group_id, image, aip::null);
+
+	//如果有可选参数
+	std::map<std::string, std::string> options;
+	options["ext_fields"] = "faceliveness";
+	options["user_top_num"] = "3";
+
+	//带参数调用人脸识别
+	result = client.identify(group_id, image, options);
+	std::cout << "face identify:" << std::endl;
+	std::cout << swriter.write(result) << std::endl;	
+	 }
+	//-------end--------------------------- 
+	
+	//--------------------------------------
+	// user info update:
+	/* {	
+	Json::Value result;
+
+	std::string uid = "YangHui";
+
+	std::string user_info = "99";
+
+	std::string group_id = "group1,group2";
+
+	std::string image;
+	Json::StyledWriter swriter;
+	aip::get_file_content("photo/WZT_227551.JPG", &image);
+
+
+	// 如果有可选参数
+	std::map<std::string, std::string> options;
+	options["action_type"] = "replace";
+
+	// 带参数调用人脸更新
+	result = client.user_update(uid, user_info, group_id, image, options);
+	std::cout << "face update:" << std::endl;
+	std::cout << swriter.write(result) << std::endl;	
+	} */
+	//-------end---------------------------
 	
 	
 	
 	
 	
-	//----------------------------------------
+	
 	/* ret = detect( detect_result, access_token_result ); 
 	cout << "detect ret =" << ret << endl;  
 	
